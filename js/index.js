0cebbf3b4340cc55a1b9fb83752419abdd5beb04
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-07-02 20:02:44 -0700

    completes number 5

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
-var arr = ['a', 'b', 'c', 'd', 'e']
+var arr = ['anaconda', 'banana', 'cake', 'dude', 'elk']
 
 console.log(arr.length)
+
+
+for (i = 0; i < arr.length; i++) {
+  console.log(arr[i].length)
+}

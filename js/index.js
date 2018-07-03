f41d5b50ea5216a82b7e78d3741d49a11f5b0b3b
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-07-02 20:23:26 -0700

    completes #13

--- js/index.js
+++ js/index.js
@@ -1,43 +1,45 @@
-var arr = ['anaconda', 'banana', 'cake', 'dude', 'elk']
+// var arr = ['anaconda', 'banana', 'cake', 'dude', 'elk']
 
-console.log(arr.length)
+// console.log(arr.length)
 
 
-for (i = 0; i < arr.length; i++) {
-  console.log(arr[i].length)
-}
+// for (i = 0; i < arr.length; i++) {
+//   console.log(arr[i].length)
+// }
 
-//This is impure function manipulating the original array
-//console.log(arr.reverse())
+// //This is impure function manipulating the original array
+// //console.log(arr.reverse())
 
-//This is another way to reverse the array using a copied array
+// //This is another way to reverse the array using a copied array
 
-// var copied = arr.slice()
+// // var copied = arr.slice()
 
-// console.log(copied.reverse())
+// // console.log(copied.reverse())
+
+// var newArr = []
+
+// for (var i = 0; i < arr.length; i++) {
+//   newArr.push(arr[i])
+// }
+
+// console.log(newArr.reverse())
+
+// console.log(newArr.sort())
+
+
+// var numArr = [12, 10, 45, 36, 88, 22]
+
+// console.log(numArr.sort((a,b)=>a-b))
+// console.log(numArr.sort((a,b)=>b-a))
+
+// var itemStrArr = ['hello', 'yes', 'okay']
+
+// itemStrArr.push('what')
+// itemStrArr.push('no')
+
+// console.log(itemStrArr)
+
+// itemStrArr.shift()
+// console.log(itemStrArr  )
 
 var newArr = []
-
-for (var i = 0; i < arr.length; i++) {
-  newArr.push(arr[i])
-}
-
-console.log(newArr.reverse())
-
-console.log(newArr.sort())
-
-
-var numArr = [12, 10, 45, 36, 88, 22]
-
-console.log(numArr.sort((a,b)=>a-b))
-console.log(numArr.sort((a,b)=>b-a))
-
-var itemStrArr = ['hello', 'yes', 'okay']
-
-itemStrArr.push('what')
-itemStrArr.push('no')
-
-console.log(itemStrArr)
-
-itemStrArr.shift()
-console.log(itemStrArr)
